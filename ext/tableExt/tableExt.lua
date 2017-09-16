a24d5d5892c0824c48a5aafefb32317580eff328
PepeAmpere <machys@gmail.com>
2017-09-17 00:14:12 +0200

    adding tableExt

---
+++ ext/tableExt/tableExt.lua
@@ -0,0 +1,85 @@
+local moduleInfo = {
+	name = "tableExt",
+	desc = "table functions extension",
+	author = "Michal Mojzik, PepeAmpere",
+	date = "2017-09-16",
+	license = "notAlicense",
+}
+
+
+-- tableExt
+-- table functions extension
+
+-- @module tableExt
+local newTableExt = {
+	-- @description Returns shallow-copy of an inserted table
+	-- @argument object [anything] source object which should be copied
+	-- @return table
+	["ShallowCopy"] = function(object)
+		if type(object) ~= 'table' then return object end
+		local newTable = setmetatable({}, getmetatable(object))
+		for k, v in pairs(object) do
+			newTable[k] = v
+		end
+		return newTable
+	end,
+	
+	-- @description Returns deep-copy of an inserted table
+	-- @argument object [anything] source table which should be copied
+	-- @argument seen [boolean|optional] set of already seen objects in the tree - used for recursion
+	-- @return table
+	["DeepCopy"] = function(object, seen)
+		if type(object) ~= 'table' then return object end
+		if seen and seen[object] then return seen[object] end
+		local s = seen or {}
+		local newTable = setmetatable({}, getmetatable(object))
+		s[object] = newTable
+		for k, v in pairs(object) do newTable[tableExt.DeepCopy(k, s)] = tableExt.DeepCopy(v, s) end
+		return newTable
+	end,
+	
+	-- @description Returns deep-copy of an inserted table
+	-- @argument object [anything] source table which should be copied
+	-- @argument maxDepth [number|optional] The maximal depth into which to serialize the table.
+	-- @comment It is preferable that the graph of the tables that are to be serialized should not contain cycles. If it does, @{dump} might end up producing duplicite output.
+	["Dump"] = function(object, maxDepth)
+		maxDepth = maxDepth or 1
+		if type(object) == 'table' then
+			if (maxDepth == 0) then 
+				return "..." 
+			end
+			--if (object.name ~= nil) then -- For outputing chili objects
+				--return object.name
+			--end
+			local s = '{ '
+			for k,v in (object.pairs or pairs)(object) do
+				 s = s .. '['..dump(k, 0)..'] = ' .. dump(v, maxDepth-1) .. ','
+			end
+			return s .. '} '
+		elseif type(object) == 'string' then
+			return string.format("%q", object):gsub("\\\n", "\\n")
+		else
+			return tostring(object)
+		end
+	end,
+	
+	-- @description Produces an alternate view of the given table that diregards `__index` metamethod.
+	-- @argument t [table] original table 
+	-- @comment This is equivalent to a situation where we would only use `rawget` on the original table. The original table is not modified in any way.
+	["Raw"] = function(t)
+		return setmetatable({}, {
+			__index = function(_, key) return rawget(t, key) end, -- retrieves only the directly stored value
+			__newindex = function() error("Attempt to write to a read-only rawTable.") end, -- disallow an writing to the resulting table
+			__pairs = function() return pairs(t) end, -- iterates only through the directly stored key-value pairs
+		})
+	end,
+}
+
+-- END OF MODULE DEFINITIONS --
+
+-- update global tables 
+if (tableExt == nil) then tableExt = {} end
+for k,v in pairs(newTableExt) do
+	if (tableExt[k] ~= nil) then Spring.Echo("NOTIFICATION: Attempt to rewrite global table in module [" .. moduleInfo.name ..  "] - key: " .. k) end
+	tableExt[k] = v 
+end
